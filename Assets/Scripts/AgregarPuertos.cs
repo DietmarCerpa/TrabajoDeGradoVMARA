fc1a2155b7b6ff22bbe4ca0343e7f58a16e18111
Jorge A. Dietmar Cerpa Plaza <jorencercas@hotmail.com>
2017-10-01 20:02:03 -0400

    Arreglo del codigo en cuanto a ORGANIZACION

--- Assets/Scripts/AgregarPuertos.cs
+++ Assets/Scripts/AgregarPuertos.cs
@@ -7,6 +7,9 @@ using System;
 
 public class AgregarPuertos : MonoBehaviour {
 
+
+    #region INICIO DEFINICION DE VARIABLES
+
     public Dropdown puertos;
     public Text Mensaje;
     public Button ConectarPuerto;
@@ -24,21 +27,51 @@ public class AgregarPuertos : MonoBehaviour {
     string[] Buffer2;
     int nivel = 0;
     SerialPort sp = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);// Refrence to serialPort 
-    
-   
-    
-    // INICIALIZACION
+
+    #endregion FIN DEFINICION DE VARIABLES
+
     void Start()
     {
+        ports = SerialPort.GetPortNames(); // Fill ports array with COM's Name available
+        puertos.options.Clear(); //clear/remove all option item
+        sp.ReadTimeout = 1; //de no encontrar nada que leer solo espera 1 ms
         
-        // Fill ports array with COM's Name available
-        ports = SerialPort.GetPortNames();
-        //clear/remove all option item
-        puertos.options.Clear();
-        sp.ReadTimeout = 1;
-        
-        //fill the dropdown menu OptionData with all COM's Name in ports[]
+        LlenarPuertos();
         //puertos.options.Add(new Dropdown.OptionData() { text= "Puertos Disponibles" });
+        //StartCoroutine(Espera());
+        //StartCoroutine(Prueba());
+    }
+
+    void Update()
+    {
+        if (sp.IsOpen == true)
+        {
+            try
+            {
+
+                int DecimalLeido = sp.ReadChar(); //lectura de dato recibido en forma Decimal
+
+                char LeidoACaracter = Convert.ToChar(Leido2); //conversion del dato de int a chr
+
+                string DecimalACadena = Convert.ToString(Leido2); // conversion del dato de int a str
+
+
+
+            }
+            catch (Exception Mensaje)
+            {
+                //Debug.Log(Mensaje);
+            }
+        }
+    }
+
+
+    #region Inicio Area de Funciones y Procedimientos
+    /// <summary>
+    /// Esta funcion permite llenar el "DROPDOWN" con los puertos SERIALES (COM) disponibles
+    /// </summary>
+    private void LlenarPuertos()
+    {
         foreach (string c in ports)
         {
             if (c == "COM3")
@@ -46,53 +79,15 @@ public class AgregarPuertos : MonoBehaviour {
                 puertos.options.Add(new Dropdown.OptionData() { text = "Sin Conexión" });
                 sp.Close();
             }
-            else puertos.options.Add(new Dropdown.OptionData() {text =  c });
+            else puertos.options.Add(new Dropdown.OptionData() { text = c });
         }
         if (sp.IsOpen == true) Debug.Log("Abierto COM 3");
         else Debug.Log("Cerrado COM 3");
-
-        //StartCoroutine(Espera());
-        //StartCoroutine(Prueba());
-
     }
-
-    IEnumerator Prueba()
-    {
-        while (true)
-        {
-            if (sp.PortName != "COM3" && sp.IsOpen)
-            {
-                // Debug.Log("entro");
-                try
-                {
-                    //Leido = sp.ReadLine();}
-                    Leido2 = sp.ReadChar();
-                    //MoveObject(sp.ReadByte());
-                    if (Leido != "")
-                    {
-                        Debug.Log(Leido);
-                    }
-                    else Debug.Log("No hay");
-                }
-
-                catch (System.Exception e)
-                {
-                    Debug.Log(e);
-                    throw;
-                }
-                yield return new WaitForSeconds(1f);
-            }
-            else
-            {
-                Debug.Log("no entro");
-                yield return new WaitForSeconds(1f);
-            }
-        }
-
-    }
-
-
-    public void Conectar() //ASIGNADO AL BOTON DE CONECTAR
+    /// <summary>
+    /// Permite ejecutar la accion de ABRIR el puerto COM seleccionado en el DROPDOWN
+    /// </summary>
+    public void Conectar() 
     {
         if (Estado == 0)
         {
@@ -101,7 +96,6 @@ public class AgregarPuertos : MonoBehaviour {
             {
                 sp.Open();
                 
-
                 mens = "Puerto " + sp.PortName + " Abierto";
                 puertos.enabled = false;
                 Conect.text = "Desconectar";
@@ -122,91 +116,12 @@ public class AgregarPuertos : MonoBehaviour {
         }
         Mensaje.text = mens;
     }
-
-
-    /*
-    void Update()
-    {
-
-        if (sp != null && sp.IsOpen)
-        {
-            StartCoroutine(Leerdatospic());
-        }
-    }
-    */
- 
-    void Update()
-    {
-        if(sp.IsOpen == true)
-        {
-            try
-            {
-                Leido2 = sp.ReadChar();
-
-
-                //CaracterLeido = Convert.ToChar(Leido2);
-
-                Lectura = Lectura + Convert.ToChar(Leido2);
-
-                Debug.Log("Lectura " + Lectura);
-                Debug.Log("Caracter Leido " + Leido2);
-
-                               
-
-                 
-            }
-            catch (Exception Mensaje)
-            {
-                //Debug.Log(Mensaje);
-            }
-           
-            
-        }
-
-
-        /*
-        if (sp.PortName != "COM3")
-        {
-            if (sp.IsOpen == true && sp.ReadLine() != null)
-            {
-                string[] values = sp.ReadLine().Split('$');
-                Mensaje.text = values[0];
-                foreach (string word in values)
-                {
-                    Mensaje.text = Mensaje + word;
-                }
-                
-            }
-        }*/
-         /*
-        if (sp.IsOpen)
-        {
-            try
-            {
-                //MoveObject(sp.ReadByte());
-                if (sp.ReadByte() != 0)
-                {
-                    print(sp.ReadByte());
-                }
-            }
-
-            catch (System.Exception e)
-            {
-                print(e);
-                throw;
-            }
-        }
-        */
-    }
-
-    IEnumerator Espera()
-    {
-        if (sp.PortName != "COM3") yield return new WaitForSeconds(0.05f);
-    }
-
-
+    /// <summary>
+    /// Funcion que permite seleccionar un puerto desde el DROPDOWN 
+    /// </summary>
+    /// <param name="index"></param>
     public void Dropdown_IndexChange(int index)
-     {
+    {
         if (ports[index] == "Sin conexión" || ports[index] == "COM3")
         {
             mens = "Elige otro";
@@ -219,37 +134,7 @@ public class AgregarPuertos : MonoBehaviour {
         }
         Mensaje.text = mens;
     }
- 
-    /*
-    private void StartCoroutine(Func<IEnumerator> leerdatospic)
-    {
-        throw new NotImplementedException();
-    }*/
+    #endregion Fin de Area de Funciones y Procedimientos
 
-
-    /*IEnumerator Leerdatospic()
-    {
-        while (true)
-        {
-            string[] values = sp.ReadLine().Split('$');
-            Mensaje.text = values[0];
-            yield return new WaitForSeconds(0.05f);
-        }
-
-    }*/
-    /*
-    IEnumerator Leerdatospic()
-    {
-        while (true)
-        {
-            string[] values = sp.ReadLine().Split('$');
-            Mensaje.text = values[0];
-            yield return new WaitForSeconds(0.05f);
-        }
-
-    }*/
 }
-
-
-
 
